3fc0b18862dca3dcee3c5a73a8fbc27399059306
Mikheil Makasarashvili <makasarashvilimikheil@gmail.com>
2023-05-06 13:39:51 +0400

    added PORT

--- db.js
+++ db.js
@@ -7,5 +7,6 @@ const db = mysql.createConnection({
   user: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE,
+  port: process.env.DATABASE_PORT,
 });
 export default db;
